a741d8fa207666956c916ddd28326f458073fae6
FelixRad <40146720160409@litec.ac.at>
2018-11-16 09:42:39 +0100

    Project solution

---
+++ CCCNov18/CCCNov18/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace CCCNov18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string path = @"C:\test.txt";
+            StreamReader reader = new StreamReader(path);
+        }
+    }
+}
